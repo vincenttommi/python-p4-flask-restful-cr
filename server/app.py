3d38aab86b68dd9123c2a01a682409f1f88373e1
vincenttommi <vincenttommi@gmail.com>
2023-06-30 03:27:21 +0300

    restful api in Flask

--- server/app.py
+++ server/app.py
@@ -1,23 +1,85 @@
-#!/usr/bin/env python3
-
-from flask import Flask, request, make_response
+from flask import Flask, jsonify,request, make_response
 from flask_migrate import Migrate
 from flask_restful import Api, Resource
 
+
+
+
 from models import db, Newsletter
 
-app = Flask(__name__)
-app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///newsletters.db'
-app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
-app.json.compact = False
 
-migrate = Migrate(app, db)
-db.init_app(app)
+app = Flask(__name__)
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///newsletters'
+app.config['SQLALCHEMY_TRACK_MODIFICATIONS']=False
+# app.config['JSONIFY_PRETTYPRINT_REGULAR']=True
+
+
+
+migrate  = Migrate(app,db)
+# nitializes the Flask-Migrate extension with your Flask application and the SQLAlchemy database object, 
+# enabling you to perform database migrations as your application's database schema evolve
 
 api = Api(app)
+#an object called api that initialise the  used api in our app
+class Index(Resource):
+    # method to get data of newsletters
+    def get(self):
+        response_dict ={
+            
+            "index":"Welcome to the Newsletter ResTful Api",
+        }  
+        response = make_response(
+            
+            jsonify(response_dict),
+            200,
+        
+        )
+        return response
+    
 
-class Home(Resource):
-    pass
+api.add_resource(Index, '/')
 
-if __name__ == '__main__':
-    app.run(port=5555, debug=True)
+class Newsletters(Resource):
+    # 
+    def get(self):
+        response_dict_list = [n.to_dict() for n in Newsletter.query.all()]       
+        
+        response  = make_response(
+            
+            jsonify(response_dict_list),
+            200,
+             
+             
+         )
+        
+        return response
+    
+    #method to post
+    
+    def  post(self):
+        new_record = Newsletters(
+            
+            
+            title=request.form['title'],
+            body=request.form['body'],
+        )
+        
+        db.session.add(new_record)
+        db.session.commit()
+        
+        
+        response_dict  = new_record.to_dict()
+        
+        response = make_response(
+            
+            jsonify(response_dict),
+            200,
+        )
+        
+        return response
+    
+api.add_resource(NewslettersByID, '/newsletters/<int:id>') 
+
+
+if __name__  == 'main':
+    app.run(port=5555)   
--- server/seed.py
+++ server/seed.py
@@ -6,12 +6,17 @@ from app import app
 from models import db, Newsletter
 
 with app.app_context():
-    
+#is typically used  when  you need to run certain code
+# within the application context,outside of the normalrequest-
+#response cycle.
     fake = Faker()
 
     Newsletter.query.delete()
+    #deletes any  data the exits in the database before fake data being seeded into
+    # table
 
     newsletters = []
+    #empty list that stores generated data
     for i in range(50):
         newsletter = Newsletter(
             title = fake.text(max_nb_chars=20),
@@ -20,4 +25,7 @@ with app.app_context():
         newsletters.append(newsletter)
 
     db.session.add_all(newsletters)
+    # adds a collection of newletter objects  to a database session.
+    
     db.session.commit()
+    #committing generated data to the database
